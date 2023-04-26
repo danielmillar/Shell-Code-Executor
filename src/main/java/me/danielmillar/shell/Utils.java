b5b0b75252b7363ccc60023d97bffe6d53f77d65
Daniel Millar <millargdaniel@gmail.com>
2023-04-26 16:00:38 +0100

    feat(): add modularity for executing shell code

--- src/main/java/me/danielmillar/shell/Utils.java
+++ src/main/java/me/danielmillar/shell/Utils.java
@@ -2,6 +2,14 @@ package me.danielmillar.shell;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Utils {
 	
@@ -10,4 +18,43 @@ public class Utils {
 		return miniMessage.deserialize(string);
 	}
 	
+	public static List<Component> colorize(List<String> strings) {
+		MiniMessage miniMessage = MiniMessage.miniMessage();
+		return strings.stream().map(miniMessage::deserialize).collect(Collectors.toList());
+	}
+	
+	public static void executeShell(CommandSender commandSender, List<String> args, boolean outputToConsole){
+		try {
+			ProcessBuilder processBuilder = new ProcessBuilder();
+			processBuilder.command(args);
+			Process process = processBuilder.start();
+			
+			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				if(outputToConsole) {
+					Bukkit.getConsoleSender().sendMessage(Utils.colorize(line));
+				}else {
+					commandSender.sendMessage(Utils.colorize(line));
+				}
+			}
+			
+			int exitCode = process.waitFor();
+			if (exitCode != 0) {
+				if(outputToConsole) {
+					Bukkit.getConsoleSender().sendMessage(Utils.colorize("<gray>Command exited with code <green>" + exitCode));
+				}else {
+					commandSender.sendMessage(Utils.colorize("<gray>Command exited with code <green>" + exitCode));
+				}
+			}
+			
+			if(outputToConsole) {
+				commandSender.sendMessage(Utils.colorize("<gray>Command executed and sent output to console. Please check console for output."));
+			}
+			
+		} catch (IOException | InterruptedException e) {
+			commandSender.sendMessage(Utils.colorize("<red>Error: <gray>Failed to execute the command. Please check console for error details."));
+			Bukkit.getLogger().warning(e.getMessage());
+		}
+	}
 }
--- src/main/java/me/danielmillar/shell/commands/ShellCommand.java
+++ src/main/java/me/danielmillar/shell/commands/ShellCommand.java
@@ -1,25 +1,18 @@
 package me.danielmillar.shell.commands;
 
 import me.danielmillar.shell.Utils;
-import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class ShellCommand implements CommandExecutor {
 	
-	private static final String PERMISSION = "shell.execute";
-	
 	@Override
 	public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-		if (!commandSender.hasPermission(PERMISSION)) {
+		if (!commandSender.hasPermission("shell.execute")) {
 			commandSender.sendMessage(Utils.colorize("<red>You do not have permission to execute this command."));
 			return true;
 		}
@@ -42,40 +35,7 @@ public class ShellCommand implements CommandExecutor {
 			return false;
 		}
 		
-		try {
-			ProcessBuilder processBuilder = new ProcessBuilder();
-			processBuilder.command(Arrays.asList(strings));
-			Process process = processBuilder.start();
-			
-			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			String line;
-			while ((line = reader.readLine()) != null) {
-				if (outputToConsole) {
-					Bukkit.getConsoleSender().sendMessage(Utils.colorize(line));
-				} else {
-					commandSender.sendMessage(Utils.colorize(line));
-				}
-			}
-			
-			int exitCode = process.waitFor();
-			if (exitCode != 0) {
-				if(outputToConsole) {
-					Bukkit.getConsoleSender().sendMessage(Utils.colorize("<gray>Command exited with code <gold>" + exitCode));
-				}else {
-					commandSender.sendMessage(Utils.colorize("<gray>Command exited with code <gold>" + exitCode));
-				}
-			}
-			
-			if(outputToConsole) {
-				commandSender.sendMessage(Utils.colorize("<gray>Command executed and sent output to console. Please check console for output."));
-			}
-			
-			return true;
-		} catch (IOException | InterruptedException e) {
-			commandSender.sendMessage(Utils.colorize("<red>Error: <gray>Failed to execute the command. Please check console for error details."));
-			Bukkit.getLogger().warning(e.getMessage());
-		}
-		
+		Utils.executeShell(commandSender, Arrays.asList(strings), outputToConsole);
 		return true;
 	}
 	
